56af8c6418352b157afba30b8764c122665caca7
Rajat986 <36686400+Rajat986@users.noreply.github.com>
2018-02-23 16:40:09 +0530

    Add files via upload

---
+++ sss.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a: "))
+b=int(input("Enter b: "))
+c=int(input("Enter c: "))
+def min():
+	if a>b and a>c:
+		print(a,"is the largest")
+	elif b>a and b>c:
+		print(b,"is the largest")
+	elif c>a and c>b:
+		print(c,"is the largest")
+
+min()
