a209c28e4681afe4433d12952ed0d87399184334
Rajat986 <36686400+Rajat986@users.noreply.github.com>
2018-03-01 19:29:17 +0530

    Update sss.py

--- sss.py
+++ sss.py
@@ -2,11 +2,11 @@ a=int(input("Enter a: "))
 b=int(input("Enter b: "))
 c=int(input("Enter c: "))
 def min():
-	if a>b and a>c:
+	if a>=b and a>=c:
 		print(a,"is the largest")
-	elif b>a and b>c:
+	elif b>=a and b>=c:
 		print(b,"is the largest")
-	elif c>a and c>b:
+	elif c>=a and c>=b:
 		print(c,"is the largest")
 
 min()
