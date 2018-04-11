28cea160d8b076c2715cd065785bafba3f04e7b8
Rajat986 <36686400+Rajat986@users.noreply.github.com>
2018-04-11 18:18:57 +0530

    Add files via upload

---
+++ bro.py
@@ -0,0 +1,56 @@
+import pdb
+pdb.set_trace()
+m=[["","",""],["","",""],["","",""]]
+
+def checkandsettox(m,a,b,x):
+    if m[a][b]=="o" or m[a][b]=="x":
+        return True
+    else:
+        m[a][b]=x
+
+def checkandsettoo(m,a,b,o):
+    if m[a][b]=="o" or m[a][b]=="x":
+        return True
+    else:
+        m[a][b]=o
+
+def checkwin(m,a,b,x):
+    checkandsettox(m,a,b,x)
+    if m[0][0]==m[0][1]==m[0][2]==x or m[1][0]==m[1][1]==m[1][2]==0 or m[2][0]==m[2][1]==m[2][2]==0 or m[0][0]==m[1][0]==m[2][0]==0 or m[0][1]==m[1][1]==m[1][2]==0 or m[2][0]==m[2][1]==m[2][2]==0 or m[0][0]==m[2][2]==m[1][1]==0 or m[1][2]==m[1][1]==m[2][0]==0:
+        return 1
+
+def computeContinue(m):
+   if m[0][0]==""or m[0][1]==""or m[0][2]==""or m[1][0]==""or m[1][1]==""or m[1][2]==""or m[2][0]==""or m[2][1]==""or m[2][2]=="":
+      return 1
+   else:
+      return 0        
+
+def printboard(m):
+    for i in m:
+        print(i)
+
+while True:
+    a=int(input("X="))
+    b=int(input("Y="))
+    r=checkandsettox(m,a,b,"x")
+    if r==True:
+        print("Overlap not allowed!!!")
+        continue
+    checkwin(m,a,b,"x")
+    if checkwin(m,a,b,"x")==1:
+        print("A wins")
+    printboard(m)
+    
+    a=int(input("X="))
+    b=int(input("Y="))
+    r=checkandsettox(m,a,b,"o")
+    if r==True:
+        print("Overlap not allowed!!!")
+        continue
+    checkwin(m,a,b,"o")
+    if checkwin(m,a,b,"o")==1:
+        print("B wins")
+    printboard(m)
+    computeContinue(m)
+    if computeContinue(m)==0:
+        exit()
