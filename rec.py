17d47e8f444b0d5fd2a40536b79d59427f03c597
Rajat986 <36686400+Rajat986@users.noreply.github.com>
2018-06-05 15:58:24 +0530

    Add files via upload
    1)If same function name in the class as the class name, if we call the same function having the class name, the function is actually called.
    2)Is it good to return or keep it as formal parameters.
    
    Return what?
    
    What user inputs should the input function return or store it in the formal parameters itself?
    What is the scope of formal parameter.
    
    
    
    What is formal parameter?
    Formal parameters are the paremeters used in the paranthesis of a function.
    Why do you need formal paramters?
    To pass the actual parameter.
    3)

---
+++ rec.py
@@ -0,0 +1,24 @@
+
+
+import math
+
+
+
+
+
+
+class point:
+    
+    def __init__(self,a,b):
+        print("Came here")
+        self.x=a
+        self.y=b
+    def distance(p):
+        d=sqrt((self.x-p.x)**2+(self.y-p.y)**2)
+        return d
+    def pointF():
+        a=int(input("Input x : "))
+        b=int(input("Input y : "))
+        return(a,b)
+    pointF()
+    
