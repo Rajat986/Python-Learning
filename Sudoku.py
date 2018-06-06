299c6ebb7c3bead7ba4c6341ea4022223e7ffeee
Rajat986 <36686400+Rajat986@users.noreply.github.com>
2018-06-06 16:07:56 +0530

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,22 +1,33 @@
-class matrix:
+class board:
     def __init__(self):
         self.a=[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+
+    def compute(self):
+        for i in range(9):
+            r=getrow(i)
+            if checkvalid(r)
+            r=getcolum(m)
         
 
-
-
+    def getrow(i):
+        return self.a[i]
+    def getcolumn(i):
+        return [ j[i] for j in self.a] 
+    def getblock(i):
+        ....
 
 
 class UI:
     def readmatrix(self):
-        m=matrix()
+        m=board()
         for i in range(9):
             for j in range(9):
                 m.a[i][j]=int(input("enter %d,%d element : "%(i,j)))
         return m
     
+
     
  
 c=UI()
-m=c.readmatrix()
+b=c.readboard()
 print(m.a)
