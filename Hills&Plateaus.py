3fd0b441b9a1f072a55cc4eb2636bb32f044609b
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2018-06-15 16:23:51 +0530

    Update Hills&Plateaus.py

--- Hills&Plateaus.py
+++ Hills&Plateaus.py
@@ -1,4 +1,4 @@
-class UI:
+lass UI:
     def input(self):
         n=int(input("Enter number of instances : "))
         d=sequences()
@@ -13,7 +13,7 @@ class UI:
             d.add_sequence(q)
         return d
 
-    def show(self,d):
+    def show(self):
         pass
 
 
@@ -28,6 +28,7 @@ class sequence:
     def add_element(self,e):
         self.ele.append(e)
         print(self.ele)
+
     def add_a(self,a):
         self.ascents.append(a)
         print(self.ascents)
@@ -42,6 +43,7 @@ class sequence:
         ctr=0
         for i in self.ele:
             if(self.ele[i] > self.ele[i+1] or self.ele[i]==self.ele[i+1]):
+                
                 ctr=ctr+1
             print("Number of ascents : %d" %ctr)
 
@@ -54,18 +56,20 @@ class sequence:
 
 class sequences:
     def __init__(self):
+        sequence()
         self.sequences=[]
     def add_sequence(self,seq):
         self.sequences.append(seq)
         print(self.sequences)
-        
+        sequence()
 
 
         
 
 w=UI()
 b=w.input()
-b.find_ascents()
-b.find_descents()
-b.find_plateaus()
+for i in b.sequences:
+    i.find_ascents()
+    i.find_descents()
+    i.find_plateaus()
 UI.show(b)
