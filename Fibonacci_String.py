f4aaf4df1115283f74c774eb1b1c94a3707aa8cc
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2018-06-18 15:20:14 +0530

    Update Fibonacci_String.py

--- Fibonacci_String.py
+++ Fibonacci_String.py
@@ -5,15 +5,19 @@ class UI:
         #n=int(input("Enter number of instances : "))
         #d=fibonacci_sequences()
         #for i in range(n):
-            q=fibonacci_sequence()
+            
             m=int(input("Enter number of strings/lines : "))
             
             s1=input("Enter 1st string : ")
-            q.add_stringS1(s1)
+            
             s2=input("Enter 2nd string : ")
-            q.add_stringS2(s2)
-            for i in range(m):
-                q.concat(s1,s2)
+            
+            q=fibonacci_sequence(s1,s2,m)
+            q.add_string(s1)
+            q.add_string(s2)
+            
+            for i in range(m-2):
+                q.concat()
                 
 
 
@@ -24,31 +28,34 @@ class UI:
                 
 
 class fibonacci_sequence:
-    def __init__(self):
-        self.s1=[]
-        self.s2=[]
+    def __init__(self,s1,s2,m):
+        self.s1=s1
+        self.s2=s2
+        self.m=m
         self.strings=[]
+        print(self.strings)
 
-    def add_stringS1(self,s):
-        self.s1.append(s)
+    def add_string(self,s):
         self.strings.append(s)
         print(self.strings)
 
-    def add_stringS2(self,s2):
-        self.s2.append(s2)
-        self.strings.append(s2)
-        print(self.strings)
+    
 
-    def concat(self,s1,s2):
-        print("String 1 : %s" %s1)
-        print("String 2 : %s" %s2)
+    def concat(self):
+        print("String 1 : %s" %self.s1)
+        print("String 2 : %s" %self.s2)
         
-        print(s1)
-        print(s2)
-        s2=s1+s2
-        print(s2)
-        self.strings.append(s2)
-
+        print(self.s1)
+        print(self.s2)
+        
+        s1=self.s2
+        s2=(self.s1)+(self.s2)
+        self.add_string(s2)
+        print(self.s2)
+        return s2
+        
+        
+    
             
 #class fibonacci_sequences:
  #   def __init__(self):
