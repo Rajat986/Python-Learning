b0ab34a0f29bdacb93c47f1c02dc4c83c5020e3b
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2018-06-21 12:42:31 +0530

    Add files via upload

---
+++ Payroll.py
@@ -0,0 +1,21 @@
+class UI:
+    def input(self):
+        n=int(input("Enter number of employees : "))
+        for i in range(n):
+            name=input("Enter name : ")
+            h_wage=float("E
+
+
+class employee:
+    def __init__(self,name,hourly_wages):
+        self.name=name
+        self.hourly_wages=hourly_wages
+        self.time_cards=[]
+
+
+class emp_time_card:
+    def __init__(self,name,hours_worked):
+        pass
+
+class payroll:
+    def 
