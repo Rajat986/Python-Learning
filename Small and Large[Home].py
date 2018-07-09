243752ce34e92696c2cb15de451fc8077b2e8f97
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2018-07-09 17:21:49 +0530

    Update Small and Large[Home].py

--- Small and Large[Home].py
+++ Small and Large[Home].py
@@ -1,22 +1,25 @@
 class UI:
-    def input_sequence():
+    def input_sequence(self):
         w=c_sequence()
-        m=int(input("Enter number of elements in sequence %d : " %(j+1)))
+        m=int(input("Enter number of elements in sequence : "))
         for i in range(m):
-                ele=int(input("Enter sequence %d element %d : " %((j+1),(i+1))))
-                w.add_ele_to_seq(ele)
+            ele=int(input("Enter element %d : " %(i+1)))
+            w.add_ele_to_seq(ele)
         return w
+            
     def input_sequences(self):
+        a=c_sequences()
         n=int(input("Enter number of sequences : "))
         for j in range(n):
             w=self.input_sequence()
-            q.add_seq_to_seqs(w)
-        return(q)
+            a.add_seq_to_seqs(w)
 
-    def output(self,inp)
+        return(a)
+        
+    def output(self,inp):
         for i in inp.sequences:
-            print("Smallest : %d \nLargest  : %d \nSecond Smallest : %d \nSecond Largest  : %d\n" % (i.small,i.large,i.s_small,i.s_large)
-                  
+            print("Smallest : %d \nLargest  : %d \nSecond Smallest : %d \nSecond Largest  : %d\n" %(i.small,i.large,i.s_small,i.s_large))
+
 class c_sequence:
     def __init__(self):
         self.sequence=[]
@@ -40,13 +43,12 @@ class c_sequence:
         # We can do the above for Largest and smallest but for 2nd smallest and largest the below is better. Initializing small, large, s_small, s_large is not needed for the below method.
         rem_repeat=set(self.sequence)
         self.large=max(rem_repeat)
-        rem_repeat.remove(large)
+        rem_repeat.remove(self.large)
         self.s_large=max(rem_repeat)
-
         self.small=min(rem_repeat)
-        rem_repeat.remove(small)
+        rem_repeat.remove(self.small)
         self.s_small=min(rem_repeat)   
-        return(small, large, s_small, s_large)
+         
 
 class c_sequences:
     def __init__(self):
@@ -54,16 +56,18 @@ class c_sequences:
 
     def add_seq_to_seqs(self,seq):
         self.sequences.append(seq)
+        print(self.sequences)
+
     def compute(self):
         for i in self.sequences:
-            result = i.result()
-    
-        
+            i.result()
 
+import pdb
 
 a=UI()
-b=a.input()
-c = b.compute()
+b=a.input_sequences()
+b.compute()
+a.output(b)
 #or
 #b.compute()
 #a.output(b)
